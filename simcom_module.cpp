33bebfbd1063e835ee9006a22b812170c104f2b1
goku-g <gokarnabaskota500@gmail.com>
2024-02-26 23:56:23 +0545

    abstract header files

---
+++ simcom_module.cpp
@@ -0,0 +1,57 @@
+#include "simcom_module.h"
+
+#include <SoftwareSerial.h>
+
+// init simcom uart object
+SoftwareSerial SimCom(RX_PIN, TX_PIN);
+
+void SIM7672X::init(uint8_t rx, uint8_t tx, uint32_t baudrate)
+{
+    Serial.println("SIMCOM Initializing...");
+
+    SimCom.begin(baudrate);
+}
+
+String SIM7672X::UARTRead(uint32_t time_out)
+{
+    Serial.println("SIMCOM's UART Reading.");
+
+    uint32_t prev_time= millis();
+    while(!(SimCom.available()) && !(millis() > prev_time + time_out)){}
+    
+    if(SimCom.available())
+    {
+        return SimCom.readString();
+    }
+    return "TimeOut";
+}
+
+uint8_t SIM7672X::runATCommand(ATCommands command, uint32_t time_out)
+{
+    uint8_t status = 0;
+    String res_buf;
+
+    Serial.println("Executing "+String(command)+" Commands.");
+
+    SIM7672X::UARTRead(time_out); // release all string from buffer!
+
+    switch(command)
+    {
+        case AT:
+            SimCom.println(F("AT"));
+            res_buf = SIM7672X::UARTRead(time_out);
+            if(res_buf.indexOf(F("OK")) != -1) status = 1;
+            break;
+
+        /* Add cases according to you needs and conditions */
+        /* USER CODE START*/
+
+        /* USER CODE END*/
+
+        default:
+            Serial.println("runATCommand() failed! Please check your command.");
+            break;
+    }
+    Serial.println(res_buf);
+    return status;
+}
---
+++ simcom_module.h
@@ -0,0 +1,25 @@
+#ifndef SIMCOM_A7672X_H
+#define SIMCOM_A7672X_H
+
+#include <Arduino.h>
+
+#define RX_PIN 0
+#define TX_PIN 1
+
+enum ATCommands
+{
+    AT = 0
+    /* Add required commands here */
+    /* USER CODE START*/
+        
+    /* USER CODE END*/
+};
+
+namespace SIM7672X
+{
+    void init(uint8_t rx, uint8_t tx, uint32_t baudrate);
+    String UARTRead(uint32_t time_out);
+    uint8_t runATCommand(ATCommands command, uint32_t time_out);
+};
+
+#endif
